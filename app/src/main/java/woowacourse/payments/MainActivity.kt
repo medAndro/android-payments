ca80f5214d8e82b1440cbb23464e519e5bcb30b3
medAndro <med4ndro@gmail.com>
2025-09-02 14:19:27 +0900

    feat: 기본 컴포넌트와 레이아웃 예제 추가

--- app/src/main/java/woowacourse/payments/MainActivity.kt
+++ app/src/main/java/woowacourse/payments/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import woowacourse.payments.ui.theme.AndroidpaymentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidpaymentsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GreetingPreview()
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding),
+//                    )
                 }
             }
         }
@@ -31,17 +48,123 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Jetpack Compose Text 실습",
+        fontSize = 22.sp,
+        color = Color.Blue,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier,
     )
+}
+
+@Composable
+fun TechCourseImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.img_techcourse),
+        contentDescription = "우아한테크코스 로고 이미지",
+    )
+}
+
+@Composable
+fun FavoriteIcon(modifier: Modifier = Modifier) {
+    Image(
+        imageVector = Icons.Filled.Favorite,
+        contentDescription = "좋아요 아이콘",
+    )
+}
+
+@Composable
+fun ButtonSave() {
+    Button(onClick = {
+        println("저장 버튼 클릭!")
+    }) {
+        Text(text = "저장")
+    }
+}
+
+@Composable
+fun ButtonHeart() {
+    Button(onClick = {
+        println("하트 버튼 클릭!")
+    }) {
+        Icon(
+            imageVector = Icons.Filled.Favorite,
+            contentDescription = "좋아요 아이콘",
+            tint = Color.Black,
+        )
+    }
+}
+
+@Composable
+fun ButtonHeartText() {
+    Button(onClick = {
+        println("하트 좋아요 텍스트 버튼 클릭!")
+    }) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = "좋아요 아이콘",
+                tint = Color.Black,
+            )
+            Text(text = "좋아요")
+        }
+    }
+}
+
+@Composable
+fun GreetingBox() {
+    Box(
+        modifier = Modifier.size(200.dp),
+    ) {
+        // 빨간 사각형, 왼쪽 위에 배치
+        Box(
+            modifier =
+                Modifier
+                    .size(50.dp)
+                    .background(Color.Red)
+                    .align(Alignment.TopStart),
+        )
+
+        // 노란 사각형, 중앙 배치
+        Box(
+            modifier =
+                Modifier
+                    .size(50.dp)
+                    .background(Color.Yellow)
+                    .align(Alignment.Center),
+        )
+
+        // 초록색 사각형, 오른쪽 아래에 배치
+        Box(
+            modifier =
+                Modifier
+                    .size(50.dp)
+                    .background(Color.Green)
+                    .align(Alignment.BottomEnd),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidpaymentsTheme {
-        Greeting("Android")
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Greeting("Android")
+            TechCourseImage()
+            FavoriteIcon()
+            ButtonSave()
+            ButtonHeart()
+            ButtonHeartText()
+            GreetingBox()
+        }
     }
 }
